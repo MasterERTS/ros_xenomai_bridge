cbfdb3cc43ac252867205e2d11d7ea6dc2fdc92e
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-06-14 21:43:02 +0200

    add thread id to echoed data

--- ros_nrt/xddp_sensors/src/xddp_sensors.cpp
+++ ros_nrt/xddp_sensors/src/xddp_sensors.cpp
@@ -9,9 +9,9 @@
 #define XDDP_PORT_LASER 0
 #define XDDP_PORT_ODOM  1
 
-float left_distance;
-float front_distance;
-float right_distance;
+float left_distance = 0.0;
+float front_distance = 0.0;
+float right_distance = 0.0;
 
 std::string laser_msg;
 std::string odom_msg;
@@ -25,9 +25,7 @@ void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
     // Split sensor data into three areas and extract smallest distance (safety)
     right_distance = *std::min_element(msg->ranges.begin(), msg->ranges.begin() + split_size);
     front_distance = *std::min_element(msg->ranges.begin() + split_size, msg->ranges.begin() + split_size*2);
-    left_distance = *std::min_element(msg->ranges.begin() + 2* split_size, msg->ranges.begin() + split_size*3);
-
-    laser_msg = "[L=" + std::to_string(left_distance) + " | F=" + std::to_string(front_distance) + " | R=" + std::to_string(right_distance) + "]";
+    left_distance = *std::min_element(msg->ranges.begin() + 2* split_size, msg->ranges.begin() + ranges_len);
 }
 
 void odomCallback(const nav_msgs::Odometry::ConstPtr& msg)
@@ -36,20 +34,19 @@ void odomCallback(const nav_msgs::Odometry::ConstPtr& msg)
     px = msg->pose.pose.position.x;
     py = msg->pose.pose.position.y;
     oz = msg->pose.pose.orientation.z;
-    
-    odom_msg = "[PX=" + std::to_string(px) + " | PY=" + std::to_string(py) + " | OZ=" + std::to_string(oz) + "]";
 }
 
 int main(int argc, char** argv) 
 {
-    ros::init(argc, argv, "sensor_exchange");
+    ros::init(argc, argv, "xddp_sensors");
     char* buffer;
+    char fwd_buffer[256];
     ros::NodeHandle nh_;
-    nh_.subscribe<sensor_msgs::LaserScan>("base_scan", 10, &laserCallback);
-    nh_.subscribe<nav_msgs::Odometry>("odom", 10, &odomCallback);
+    ros::Subscriber subLaser = nh_.subscribe<sensor_msgs::LaserScan>("/base_scan", 10, &laserCallback);
+    //ros::Subscriber subOdometry = nh_.subscribe<nav_msgs::Odometry>("odom", 10, &odomCallback);
     
-    ChatterXDDP laser_chatter("/laser_nrt", 0);
-    ChatterXDDP odom_chatter("/odom_nrt", 1);
+    ChatterXDDP laser_chatter("/laser_nrt", XDDP_PORT_LASER);
+    //ChatterXDDP odom_chatter("/odom_nrt", XDDP_PORT_ODOM);
 
     std::size_t laser_fail_count = 0, odom_fail_count = 0;
 
@@ -57,21 +54,28 @@ int main(int argc, char** argv)
 
     while (ros::ok())
     {
-        laser_chatter.nrt_thread_write(&laser_msg[0]);
+        sprintf(fwd_buffer, "[%f | %f | %f]", left_distance, front_distance, right_distance);
+        laser_chatter.nrt_thread_write(fwd_buffer);
         buffer = laser_chatter.nrt_thread_read();
-        if (buffer != "ack")
+        if (!(buffer[0] == 'a' && buffer[1] == 'c' && buffer[2] == 'k'))
         {
-            ROS_INFO("Laser Data Failed to be acknowledged");
             laser_fail_count++;
+            if (laser_fail_count % 10 == 0){
+                ROS_INFO("Laser Data Failed to be acknowledged %d times !!", laser_fail_count);
+            } else {
+                ROS_INFO("RT Thread |ACK| received...")
+            }
         }
-
-        odom_chatter.nrt_thread_write(&odom_msg[0]);
+        /*
+        sprintf(fwd_buffer, "[%f | %f | %f]", px, py, oz);
+        odom_chatter.nrt_thread_write(fwd_buffer);
         buffer = odom_chatter.nrt_thread_read();
         if (buffer != "ack") 
         {
             ROS_INFO("Laser Data Failed to be acknowledged");
             odom_fail_count++;
         }
+        */
         ros::spinOnce();
         loop_rate.sleep();
     }
